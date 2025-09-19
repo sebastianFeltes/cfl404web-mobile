6ca3dadf14789db99dbbfbfa42b316c6b292b4c9
sebastianFeltes <jikansui16@gmail.com>
2025-09-19 10:22:33 -0300

    first commit

---
+++ constants/Colors..ts
@@ -0,0 +1,10 @@
+export const Colors = {
+  customWhite: "#fff",
+  customRed: "#bf0411",
+  customBlack: "#000000",
+  customGray: "#6a6a6a",
+  customLigthBlue: "#37a5dd",
+  customDarkBlue: "#166193",
+  customSoftBlue: "#8fd8fd",
+  fontPoppins: "Poppins",
+};
